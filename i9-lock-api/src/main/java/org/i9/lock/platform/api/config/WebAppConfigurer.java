c32ecee6a9a3048236c77a8464d6616d2713f432
vinohsueh <413311010@qq.com>
2018-06-12 15:23:18 +0800

    1

--- i9-lock-api/src/main/java/org/i9/lock/platform/api/config/WebAppConfigurer.java
+++ i9-lock-api/src/main/java/org/i9/lock/platform/api/config/WebAppConfigurer.java
@@ -39,7 +39,7 @@ public class WebAppConfigurer extends WebMvcConfigurerAdapter {
 	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		
+		registry.addResourceHandler("/source/**").addResourceLocations("file:///usr/local/lockPic/");
 //		registry.addResourceHandler("/myres/**").addResourceLocations("classpath:/myres/");
 //		registry.addResourceHandler("/**").addResourceLocations("/");
 		super.addResourceHandlers(registry);
